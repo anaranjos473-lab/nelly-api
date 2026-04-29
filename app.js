41207b7a48b3a7d1b9987e8b64530891e77d859e
hp14 <hp14@example.com>
2026-04-29 15:17:58 -0600

    Fix rutas a src/routes

--- app.js
+++ app.js
@@ -38,9 +38,9 @@ if (db) {
 }
 
 // 3. RUTA DE ÓRDENES (expuesta para el monitor y pruebas)
-import usuariosRouter from './routes/users.js';
-import ordenesRouter from './routes/orders.js';
-import zonasRouter from './routes/zones.js';
+import usuariosRouter from './src/routes/users.js';
+import ordenesRouter from './src/routes/orders.js';
+import zonasRouter from './src/routes/zones.js';
 
 // Vinculación de rutas principales
 app.use('/api/usuarios', usuariosRouter);
